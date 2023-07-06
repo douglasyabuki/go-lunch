4876e168a0b1f70d36eb18164a732a586d8b63d3
Douglas Yuji Yabuki <douglasyabuki@gmail.com>
2023-07-06 15:04:27 -0300

    Commit 14: Add React Toastify to dependencies

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -3,8 +3,7 @@ import Link from "next/link";
 export default function Home() {
   return (
     <main className={`flex min-h-screen flex-col items-center justify-between p-24`}>
-      <h1>Hello World!</h1>
-      <Link href={"/authentication"}>Go to Auth</Link>
+      <h1>You are authenticated!</h1>
     </main>
   );
 }
