0898354e727aca9dfea02431bd0760432c760c72
Douglas Yuji Yabuki <douglasyabuki@gmail.com>
2023-07-07 09:47:12 -0300

    Commit 20: Minor changes in layout

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,7 +2,7 @@ import Link from "next/link";
 
 export default function Home() {
   return (
-    <main className={`flex w-screen min-h-screen flex-col items-center justify-between p-24 bg-background-black text-not-so-white`}>
+    <main className={`flex w-screen min-h-screen flex-col items-center justify-between p-24 dark:bg-background-black dark:text-not-so-white transform-gpu transition-colors duration-300`}>
       <h1>You are authenticated!</h1>
     </main>
   );
