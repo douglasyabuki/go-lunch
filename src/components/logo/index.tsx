472d07d13c473f13240e80e66628573ae9da2dc8
Douglas Yuji Yabuki <douglasyabuki@gmail.com>
2023-07-07 09:31:27 -0300

    Commit 19: Adjustments in next Img

--- src/components/logo/index.tsx
+++ src/components/logo/index.tsx
@@ -4,14 +4,17 @@ export interface LogoProps {}
 export default function Logo(props: LogoProps) {
   return (
     <div className="flex text-xl text-not-so-white items-center align-middle h-16">
-      <Image
-        src="./plate-white.svg"
-        alt="SVG from w3"
-        width={64}
-        height={64}
-        className="mr-1"
-      />
-      <h1 className="text-not-so-white text-4xl font-extrabold mr-1">go</h1>
+      <div className="relative h-16 w-16 mr-2">
+        <Image
+          src="./plate-white.svg"
+          alt="SVG from w3"
+          fill
+          style={{
+            objectFit: "cover",
+          }}
+        />
+      </div>
+      <h1 className="text-not-so-white text-4xl font-extrabold mr-[2px]">go</h1>
       <h1 className="text-primary-green text-4xl font-extrabold">lunch</h1>
     </div>
   );
--- src/components/navbar/index.tsx
+++ src/components/navbar/index.tsx
@@ -10,16 +10,18 @@ import ThemeSwitcher from "./theme-switcher/ThemeSwitcher";
 
 /*  Contexts  */
 import { AuthContext } from "@/contexts/AuthContext";
+import { ThemeContext } from "@/contexts/ThemeContext";
 
-export interface NavbarProps {}
-
-export default function Navbar(props: NavbarProps) {
+export default function Navbar() {
   const { isAuthenticated } = useContext(AuthContext);
+  const { darkMode } = useContext(ThemeContext);
+
+  let currentStyle = darkMode === true ? "bg-gradient-to-b from-background-black to-transparent" :  "bg-background-div1"
 
   return (
     <>
       {isAuthenticated ? (
-        <header className="group container items-center fixed top-0 z-50 flex h-11 w-screen min-w-full justify-between bg-gradient-to-b from-background-black to-transparent px-8 py-1 text-center text-not-so-white duration-500 hover:bg-background-black sm:px-14 md:h-16 md:px-16 md:py-2 lg:px-20 xl:px-32 shadow-lg shadow-translucid-black backdrop-blur-md">
+        <header className={`${currentStyle} group container items-center fixed top-0 z-50 flex h-11 w-screen min-w-full justify-between  px-8 py-1 text-center text-not-so-white duration-500 hover:bg-background-black sm:px-14 md:h-16 md:px-16 md:py-2 lg:px-20 xl:px-32 shadow-lg shadow-translucid-black backdrop-blur-md`}>
           <Logo></Logo>
           <ThemeSwitcher></ThemeSwitcher>
         </header>
